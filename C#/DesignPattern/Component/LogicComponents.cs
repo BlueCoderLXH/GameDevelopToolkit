7f38c6afd577099f0c8c336827d901e494d06719
黎小华 <bluecoder@yeah.net>
2019-03-14 15:37:01 +0800

    Pattern
    1、Component Pattern
    2、ServiceLocate Pattern

---
+++ C#/DesignPattern/Component/LogicComponents.cs
@@ -0,0 +1,111 @@
+﻿namespace Framework.LogicComponent
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// 游戏逻辑组件接口
+    /// </summary>
+    public interface ILogicComponent
+    {
+        //void OnInit();
+        void OnUpdate(float dt);
+        void OnDestroy();
+    }
+
+    /// <summary>
+    /// 游戏逻辑组件集合
+    /// 采用组件设计模式(Component Pattern)实现
+    /// </summary>
+    public class LogicComponents
+    {
+        Dictionary<Type, ILogicComponent> m_Components;
+
+        /// <summary>
+        /// 构造
+        /// </summary>
+        /// <param name="capacity">组件数</param>
+        public LogicComponents(int capacity)
+        {
+            if (capacity <= 0)
+            {
+                capacity = 1;
+            }
+
+            m_Components = new Dictionary<Type, ILogicComponent>(capacity);
+        }
+
+        /// <summary>
+        /// 添加组件
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        public void AddComponent<T>() where T : ILogicComponent, new()
+        {
+            Type compType = typeof(T);
+
+            if (!m_Components.ContainsKey(compType))
+            {
+                T component = new T();
+
+                m_Components.Add(compType, component);
+            }
+        }
+
+        /// <summary>
+        /// 添加组件
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="component"></param>
+        public void AddCompnent<T>(T component) where T : ILogicComponent
+        {
+            Type compType = component.GetType();
+
+            if (!m_Components.ContainsKey(compType))
+            {
+                m_Components.Add(compType, component);
+            }
+        }
+
+        /// <summary>
+        /// 获取组件
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public T GetComponent<T>() where T : class, ILogicComponent
+        {
+            Type compType = typeof(T);
+
+            ILogicComponent component = null;
+
+            if (!m_Components.TryGetValue(compType, out component))
+            {
+                return default(T);
+            }
+
+            return component as T;
+        }
+
+        /// <summary>
+        /// 更新所有组件
+        /// </summary>
+        /// <param name="dt"></param>
+        public void Update(float dt)
+        {
+            foreach (var component in m_Components)
+            {
+                component.Value.OnUpdate(dt);
+            }
+        }
+
+        /// <summary>
+        /// 释放所有的组件
+        /// </summary>
+        public void Destroy()
+        {
+            foreach (var component in m_Components)
+            {
+                component.Value.OnDestroy();
+            }
+        }
+    }
+}
---
+++ C#/DesignPattern/ServiceLocator/ServiceLocator.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Framework.Service
+{
+    /// <summary>
+    /// ServiceLocator的抽象接口
+    /// </summary>
+    public interface ICommonService
+    {
+        void Release();
+    }
+
+    /// <summary>
+    /// 服务定位器
+    /// 1、凡是全局用到的服务, 都可以通过注入到ServiceLocator中, 减少项目中单例的个数
+    /// 2、服务不能重复注入
+    /// 3、ServiceLocator作为通用的框架代码, 不能直接使用, 使用需继承其之
+    /// </summary>
+    public abstract class ServiceLocator
+    {
+        private Dictionary<Type, ICommonService> m_Services;
+
+        public ServiceLocator()
+        {
+            m_Services = new Dictionary<Type, ICommonService>();
+        }
+
+        private ICommonService Get(Type serviceType)
+        {
+            ICommonService service = null;
+
+            if (!m_Services.TryGetValue(serviceType, out service))
+            {
+                return null;
+            }
+
+            return service;
+        }
+
+        /// <summary>
+        /// 获取服务
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public T Get<T>() where T : class, ICommonService
+        {
+            ICommonService service = Get(typeof(T));
+
+            if (service == null)
+            {
+                SolarLogger.LogWarningFormat(eOutPutModule.General, "ServiceLocator type '{0}' is not found!", typeof(T));
+                return null;
+            }
+
+            return service as T;
+        }
+
+        /// <summary>
+        /// 注入服务
+        /// </summary>
+        /// <param name="registerService"></param>
+        public void Register<T>() where T : class, ICommonService
+        {
+            Type serviceType = typeof(T);
+
+            if (Get(serviceType) == null)
+            {
+                ICommonService registerService = ServiceMaker.CreateService<T>();
+
+                m_Services.Add(serviceType, registerService);
+            }
+        }
+
+        /// <summary>
+        /// 注入服务对象
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="service"></param>
+        public void Register<T>(T service) where T : class, ICommonService
+        {
+            Type serviceType = service.GetType();
+
+            if (Get(serviceType) == null)
+            {
+                m_Services.Add(serviceType, service);
+            }
+        }
+
+        /// <summary>
+        /// 注销服务
+        /// </summary>
+        /// <param name="registerService"></param>
+        public void Unregister<T>() where T : ICommonService
+        {
+            Type serviceType = typeof(T);
+
+            ICommonService service = Get(serviceType);
+
+            if (service != null)
+            {
+                service.Release();
+                m_Services.Remove(serviceType);
+            }
+        }
+
+        /// <summary>
+        /// 释放所有的服务对象
+        /// (退出游戏时调用)
+        /// </summary>
+        public void Release()
+        {
+            foreach (var service in m_Services)
+            {
+                service.Value.Release();
+            }
+
+            m_Services.Clear();
+        }
+    }
+}
