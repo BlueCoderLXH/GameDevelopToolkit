5563f4081eb0980b15cf4619d0ea21ee95df201b
黎小华 <lixiaohua2@kingsoft.com>
2023-03-01 15:46:43 +0800

    全局事件系统： - TMap::FindOrAdd返回的是对象的引用，非指针，直接使用=赋值操作会触发一次拷贝，代理对象的拷贝在特殊情况下会导致内存重组，以使得事件无法正常触发 - 编辑器上listener不区分netmode，需要区分的在编辑器上用进程分离

--- UE4/Framework/GameEventSystem/Source/GameEventSystem/Private/GameEventHandler.cpp
+++ UE4/Framework/GameEventSystem/Source/GameEventSystem/Private/GameEventHandler.cpp
@@ -1,74 +1,59 @@
 #include "GameEventHandler.h"
 
 //---------- FGameEventListeners ----------
-bool FGameEventListeners::Dispatch(const FGameEventData& EventData)
-{
-	if (!GetListener().IsBound())
-	{
-		return false;
-	}
+bool FGameEventListeners::Dispatch(const FGameEventData& EventData) {
+    if (!GetListener().IsBound()) {
+        return false;
+    }
 
-	GetListener().Broadcast(EventData);
-	return true;
+    GetListener().Broadcast(EventData);
+    return true;
 }
 
-void FGameEventListeners::Clear()
-{
-#ifdef UE_EDITOR
-	ListenersOnDS.Clear();
-#endif
-
-	Listeners.Clear();
-	HandleMap.Empty();
+void FGameEventListeners::Clear() {
+    Listeners.Clear();
+    HandleMap.Empty();
 }
 
 //---------- FGameEventHandler ----------
 bool FGameEventHandler::Register(
-	const FGameEventType& InEventID,
-	const FGameEventStaticDelegate InStaticCallback)
-{
-	auto ListenersPtr = EventMap.FindOrAdd(InEventID);
-	ListenersPtr.Init(InEventID);
-	
-	if (!ListenersPtr.Register(InStaticCallback))
-	{
-		return false;
-	}
+    const FGameEventType& InEventID,
+    const FGameEventStaticDelegate InStaticCallback) {
+    
+    FGameEventListeners& ListenersPtr = EventMap.FindOrAdd(InEventID);
+    ListenersPtr.Init(InEventID);
 
-	EventMap[InEventID] = ListenersPtr;
-	return true;	
+    if (!ListenersPtr.Register(InStaticCallback)) {
+        return false;
+    }
+    
+    return true;
 }
 
 bool FGameEventHandler::Unregister(
-	const FGameEventType& InEventID,
-	const FGameEventStaticDelegate InStaticCallback)
-{
-	auto ListenersPtr = EventMap.Find(InEventID);
-	if (!ListenersPtr)
-	{
-		return false;
-	}
-	
-	return ListenersPtr->Unregister(InStaticCallback);
+    const FGameEventType& InEventID,
+    const FGameEventStaticDelegate InStaticCallback) {
+    FGameEventListeners* ListenersPtr = EventMap.Find(InEventID);
+    if (!ListenersPtr) {
+        return false;
+    }
+
+    return ListenersPtr->Unregister(InStaticCallback);
 }
 
-bool FGameEventHandler::Dispatch(const FGameEventData& EventData)
-{	
-	const auto ListenersPtr = EventMap.Find(EventData.EventID);
-	if (!ListenersPtr)
-	{
-		return false;
-	}
+bool FGameEventHandler::Dispatch(const FGameEventData& EventData) {
+    const auto ListenersPtr = EventMap.Find(EventData.EventID);
+    if (!ListenersPtr) {
+        return false;
+    }
 
-	return ListenersPtr->Dispatch(EventData);
+    return ListenersPtr->Dispatch(EventData);
 }
 
-void FGameEventHandler::Clear()
-{
-	for (auto Listeners : EventMap)
-	{
-		Listeners.Value.Clear();
-	}
-	
-	EventMap.Empty();
+void FGameEventHandler::Clear() {
+    for (auto Listeners : EventMap) {
+        Listeners.Value.Clear();
+    }
+
+    EventMap.Empty();
 }
