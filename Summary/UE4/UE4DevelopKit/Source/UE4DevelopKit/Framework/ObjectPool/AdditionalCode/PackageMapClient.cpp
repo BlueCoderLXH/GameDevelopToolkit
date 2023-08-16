7648e544becbb89e66a77723a7b95d4cc4cd0012
lixiaohua <bluecoder@yeah.net>
2023-08-16 10:55:51 +0800

    [ObjectPoolSystem] Remove annotation

--- Summary/UE4/UE4DevelopKit/Source/UE4DevelopKit/Framework/ObjectPool/AdditionalCode/PackageMapClient.cpp
+++ Summary/UE4/UE4DevelopKit/Source/UE4DevelopKit/Framework/ObjectPool/AdditionalCode/PackageMapClient.cpp
@@ -1,34 +1,34 @@
-// void UPackageMapClient::ResetGuidAckStatus(UObject* InObj) const
-// {
-// 	if (!IsValid(InObj))
-// 	{
-// 		UE_LOG(LogNetPackageMap, Error, TEXT("ResetGuidAckStatus: InObj is invalid!"));
-// 		return;
-// 	}
-//
-// 	const FNetworkGUID* ObjNetGuid = GuidCache->NetGUIDLookup.Find(InObj);
-// 	if (!ObjNetGuid)
-// 	{
-// 		UE_LOG(LogNetPackageMap, Error, TEXT("ResetGuidAckStatus: NetGUIDLookup don't contains Obj:%s!"), *(InObj->GetFullName()));
-// 		return;
-// 	}
-//
-// 	ResetGuidAckStatus(*ObjNetGuid);
-// }
-//
-// void UPackageMapClient::ResetGuidAckStatus(const FNetworkGUID& InNetworkGUID) const
-// {
-// 	if (!InNetworkGUID.IsValid())
-// 	{
-// 		UE_LOG(LogNetPackageMap, Error, TEXT("ResetGuidAckStatus: InNetworkGUID:%s is invalid!"), *(InNetworkGUID.ToString()));
-// 		return;
-// 	}
-//
-// 	int32* ExpectedNetGUIDAckStatusPtr = OverrideAckState->NetGUIDAckStatus.Find(InNetworkGUID);
-// 	// if acked, reset it to be not acked
-// 	// if pending or not acked, ignore
-// 	if (ExpectedNetGUIDAckStatusPtr && *ExpectedNetGUIDAckStatusPtr == GUID_PACKET_ACKED)
-// 	{
-// 		*ExpectedNetGUIDAckStatusPtr = GUID_PACKET_NOT_ACKED;
-// 	}
-// }
+void UPackageMapClient::ResetGuidAckStatus(UObject* InObj) const
+{
+	if (!IsValid(InObj))
+	{
+		UE_LOG(LogNetPackageMap, Error, TEXT("ResetGuidAckStatus: InObj is invalid!"));
+		return;
+	}
+
+	const FNetworkGUID* ObjNetGuid = GuidCache->NetGUIDLookup.Find(InObj);
+	if (!ObjNetGuid)
+	{
+		UE_LOG(LogNetPackageMap, Error, TEXT("ResetGuidAckStatus: NetGUIDLookup don't contains Obj:%s!"), *(InObj->GetFullName()));
+		return;
+	}
+
+	ResetGuidAckStatus(*ObjNetGuid);
+}
+
+void UPackageMapClient::ResetGuidAckStatus(const FNetworkGUID& InNetworkGUID) const
+{
+	if (!InNetworkGUID.IsValid())
+	{
+		UE_LOG(LogNetPackageMap, Error, TEXT("ResetGuidAckStatus: InNetworkGUID:%s is invalid!"), *(InNetworkGUID.ToString()));
+		return;
+	}
+
+	int32* ExpectedNetGUIDAckStatusPtr = OverrideAckState->NetGUIDAckStatus.Find(InNetworkGUID);
+	// if acked, reset it to be not acked
+	// if pending or not acked, ignore
+	if (ExpectedNetGUIDAckStatusPtr && *ExpectedNetGUIDAckStatusPtr == GUID_PACKET_ACKED)
+	{
+		*ExpectedNetGUIDAckStatusPtr = GUID_PACKET_NOT_ACKED;
+	}
+}
--- Summary/UE4/UE4DevelopKit/Source/UE4DevelopKit/Framework/ObjectPool/AdditionalCode/PackageMapClient.h
+++ Summary/UE4/UE4DevelopKit/Source/UE4DevelopKit/Framework/ObjectPool/AdditionalCode/PackageMapClient.h
@@ -1,6 +1,6 @@
-// UCLASS(transient)
-// class ENGINE_API UPackageMapClient : public UPackageMap
-// {
-// 	void ResetGuidAckStatus(UObject* InObj) const;
-// 	void ResetGuidAckStatus(const FNetworkGUID& InNetworkGUID) const;	
-// }
+UCLASS(transient)
+class ENGINE_API UPackageMapClient : public UPackageMap
+{
+	void ResetGuidAckStatus(UObject* InObj) const;
+	void ResetGuidAckStatus(const FNetworkGUID& InNetworkGUID) const;	
+}
